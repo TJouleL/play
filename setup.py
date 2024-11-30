a1c48a557f56112d56abc684828c943bbd9d3d3f
Koen <98043234+koen1711@users.noreply.github.com>
2024-11-30 17:18:27 +0100

    chore: fix required python version in accordance with pymunk
    Signed-off-by: Koen <98043234+koen1711@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    python_requires=">=3.5",
+    python_requires=">=3.10",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["pygame", "numpy", "pymunk"],
