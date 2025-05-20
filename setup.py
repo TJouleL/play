d65caefb27c01a573b516e9b444105722df693fc
Koen <98043234+koen1711@users.noreply.github.com>
2025-05-20 08:39:25 +0200

    fix: setup.py `install_requires` (#85)
    * fix: setup.py `install_requires`
    
    Signed-off-by: Koen <98043234+koen1711@users.noreply.github.com>
    
    * chore: bump version
    
    Signed-off-by: Koen <98043234+koen1711@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Koen <98043234+koen1711@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -7,10 +7,13 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 # This call to setup() does all the work
 setup(
     name="corderius-play",
-    version="2.6.0",
+    version="2.6.1",
     description="The easiest way to make games and media projects in Python.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -26,5 +29,5 @@ setup(
     python_requires=">=3.10",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["pygame", "numpy", "pymunk", "screeninfo"],
+    install_requires=required,
 )
