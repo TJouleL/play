0f61a77a2720e03b8984d342d6b9813da24dd760
koen1711 <koenvurk1711@gmail.com>
2025-06-09 14:23:44 +0200

    chore: black + pylint

--- play/__init__.py
+++ play/__init__.py
@@ -1,12 +1,12 @@
 """The library to make pygame easier to use."""
 
+import warnings
 import pygame
 
-pygame.init()
 
-import warnings
 from .api import *
 from .io.controllers import controllers
 from .io.mouse import mouse
 
+pygame.init()
 warnings.filterwarnings("ignore", category=UserWarning, module=r"cffi\.cparser")
